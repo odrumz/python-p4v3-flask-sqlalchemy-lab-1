c8dfe882990700c76f06574ba0bf90437d002b3b
odrumz <odrumz64gmail.com>
2024-06-29 22:48:13 +0300

    lab1

--- server/app.py
+++ server/app.py
@@ -21,6 +21,43 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def locate_id(id):
+    locate = Earthquake.query.filter_by(id=id).first()
+
+    if locate:
+        body = locate.to_dict()
+        status = 200
+
+    else:
+        body = {'message':f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    matching_quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    count = len(matching_quakes)
+    quakes_data = []
+
+    for quake in matching_quakes:
+        quakes_data.append({
+            'id': quake.id,
+            'location': quake.location,
+            'magnitude': quake.magnitude,
+            'year': quake.year
+        })
+
+    response_body = {
+        'count': count,
+        'quakes': quakes_data
+    }
+
+    return make_response(response_body, 200)
+ 
 
 
 if __name__ == '__main__':
